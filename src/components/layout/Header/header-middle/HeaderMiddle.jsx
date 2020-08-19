33c40aa4d282fff7da83100f6b770fd10d20aca7
rohan <rohanrajgautam@gmail.com>
2020-08-19 15:35:25 +0545

    added a hover popover in stores in header middle section

--- src/components/layout/Header/header-middle/HeaderMiddle.jsx
+++ src/components/layout/Header/header-middle/HeaderMiddle.jsx
@@ -7,6 +7,15 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 
+//hover pop up
+import Popover from 'material-ui-popup-state/HoverPopover';
+import {
+  usePopupState,
+  bindHover,
+  bindPopover,
+} from 'material-ui-popup-state/hooks';
+import Typography from '@material-ui/core/Typography';
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -16,10 +25,26 @@ const useStyles = makeStyles((theme) => ({
   logo: {
     flexGrow: 1,
   },
+  popover: {
+    background: '#ffffff',
+    width: '900px',
+    height: '250px',
+    margin: 10,
+  },
+  popoverTitle: {
+    margin: 10,
+    borderBottom: '1px solid gray',
+    padding: '10px 0',
+    fontWeight: 'bold',
+  },
 }));
 
 const HeaderMiddle = () => {
   const classes = useStyles();
+  const popupState = usePopupState({
+    variant: 'popover',
+    popupId: 'demoPopover',
+  });
 
   return (
     <div className={classes.root}>
@@ -33,9 +58,30 @@ const HeaderMiddle = () => {
         <Button component={Link} to='/deals' disableRipple>
           <b>Deals</b>
         </Button>
-        <Button disableRipple>
+        <Button disableRipple {...bindHover(popupState)}>
           <b>Stores</b>
         </Button>
+
+        {/* hover popover for stores */}
+
+        <Popover
+          {...bindPopover(popupState)}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+          disableRestoreFocus
+        >
+          <div className={classes.popover}>
+            <Typography className={classes.popoverTitle}>
+              Find coupons by Stores
+            </Typography>
+          </div>
+        </Popover>
         <Button disableRipple>
           <b>Forums</b>
         </Button>
