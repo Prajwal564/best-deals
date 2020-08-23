8d0c88458aa9b90641b7d197be5fc71b732634bf
prajwal <sharma.prajwal.564@gmail.com>
2020-08-23 20:55:31 +0545

    added side drawer

--- src/components/layout/Header/Header-bottom/HeaderBottom.jsx
+++ src/components/layout/Header/Header-bottom/HeaderBottom.jsx
@@ -130,12 +130,6 @@ export default function HeaderBottom() {
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleDrawerOpen = () => {
     setOpens(!opens);
   };
 
